80290ea3d30ef5c04774ef74f99376529e2f7e56
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-07 22:36:13 +0200

    Create Exemple.py

---
+++ Exemple.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+
+intent=discord.Intents(messages=True,guilds=True,members=True,bans=False,emojis=False,integrations=False,webhooks=False,invites=False,voice_states=True, reactions=True)
+bot=commands.Bot(command_prefix=(commands.when_mentioned_or('OT?')), case_insensitive=True, intents=intent,chunk_guilds_at_startup=True)
+token=""
+
+@bot.event 
+async def on_ready():
+    pass
+
+@bot.listen()
+async def on_message(message):
+    pass
+
+@bot.listen()
+async def on_message_delete(message):
+    pass
+
+@bot.listen()
+async def on_reaction_add(reaction,user):
+    pass
+
+@bot.listen()
+async def on_raw_message_edit(payload):
+    pass
+
+@bot.event
+async def on_command(ctx):
+    pass
+
+class Test(commands.Cog):
+    @commands.command()
+    async def test1(self,ctx,*args):
+        pass
+    @commands.command()
+    async def test2(self,ctx,*args):
+        pass
+    @commands.command()
+    async def test2(self,ctx,*args):
+        pass
+
+bot.run(token)
