a5e6dbd451addf9bdcf0749a7da8843a41e89f2d
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-10 19:02:42 +0200

    Problème nouveau membre
    Erreur qui faisait que les nouveaux membres n'étaient pas enregistrés, et faisait buguer les classements.

--- Stats/SQL/NewMembres.py
+++ Stats/SQL/NewMembres.py
@@ -2,7 +2,7 @@ from Stats.SQL.ConnectSQL import connectSQL
 
 def leaveUser(guild,user,state):
     connexion,curseur=connectSQL(guild.id,"Guild","Guild",None,None)
-    if curseur.execute("SELECT * FROM users WHERE ID={0}".format(user.id))==None:
+    if curseur.execute("SELECT * FROM users WHERE ID={0}".format(user.id)).fetchone()==None:
         curseur.execute("INSERT INTO users VALUES({0},{1},{2},{3})".format(user.id,False,False,False))
     curseur.execute("UPDATE users SET Leave={0} WHERE ID={1}".format(state,user.id))
     connexion.commit()
