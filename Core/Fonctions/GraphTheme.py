8c6fbc09d75e925b9892a43b074e56036724230a
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-12 12:34:50 +0200

    Tentative correction bug d'affichage graphiques

--- Core/Fonctions/GraphTheme.py
+++ Core/Fonctions/GraphTheme.py
@@ -1,21 +1,9 @@
 from time import strftime
+from random import choice
 
-"""tempStyle="seaborn-darkgrid"
-def setThemeGraph(plt):
-    global tempStyle
-    if int(strftime("%H"))>8 and int(strftime("%H"))<22:
-        if tempStyle!="seaborn-darkgrid":
-            from matplotlib import pyplot as plt
-            tempStyle="seaborn-darkgrid"
-        plt.style.use("seaborn-darkgrid")
-    else:
-        if tempStyle!="dark_background":
-            from matplotlib import pyplot as plt
-            tempStyle="dark_background"
-        plt.style.use("dark_background")"""
-
-def setThemeGraph(plt):
+def setThemeGraphLOl(plt):
     """Change le thème du graphique en fonction de s'il fait nuit ou jour"""
+    plt.rcdefaults()
     if int(strftime("%H"))>8 and int(strftime("%H"))<22:
         plt.style.use("seaborn-darkgrid")
     else:
