8ba2f4a15f6a4c08ec11fef8dc4aaf282c56dbf6
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-19 18:47:20 +0200

    Mise à jour des statuts
    Désormais prend les descriptions des commandes et en choisit une aléatoirement pour l'afficher en statut du bot.

--- Core/Fonctions/Statut.py
+++ Core/Fonctions/Statut.py
@@ -1,16 +1,14 @@
-import discord
-from random import choice
 import asyncio
-from Fonctions.EcritureRecherche3 import rechercheCsv
+from random import choice
 
-async def changepres(active,client):
-    '''Changement de statut toutes les 150secondes'''
-    pres=rechercheCsv("pres",0,0,0,0,0)[0]
-    while active:
-        i=choice(pres)
-        await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=i["Statut"]))
-        # await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="OT!"+commands[i]+" : "+pres[i]+" | OT!help"))
-        if i["Statut"]=="J'aime Zey.":
-            await asyncio.sleep(5)
-        else:
-            await asyncio.sleep(150)
+import discord
+
+
+async def changeStatut(bot):
+    '''Changement de statut toutes les deux minutes'''
+    listeStatut=[]
+    for i in bot.commands:
+        listeStatut.append("OT!help | OT!{0} : {1}".format(i.name, i.help))
+    while True:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=choice(listeStatut)))
+        await asyncio.sleep(120)
