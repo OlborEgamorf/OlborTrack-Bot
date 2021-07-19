bb4376afac92eb1cd150255800499e253fbc557d
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-19 17:38:08 +0200

    Déplacement de fichier
    Zip étant une commande admin, passage dans le dossier Admin

--- Admin/Zip.py
+++ Admin/Zip.py
@@ -3,20 +3,20 @@
 import zipfile
 import discord
 import os
+from Core.Fonctions.Embeds import createEmbed
 
 async def exeZip(ctx,bot):
     """Cette fonction regroupe les stats du serveur dans un fichier .zip et l'envoie, pour pouvoir les consulter.
     
     La commande a un cooldown élevé."""
-    ziph = zipfile.ZipFile("_"+str(ctx.guild.id)+".zip", 'w', zipfile.ZIP_DEFLATED)
-    embedF=discord.Embed(title="<:otVERT:718396570638483538> Demande en cours d'exécution.", description="Vous allez recevoir vos données par message privé dans quelques instants.", color=0x339966)
-    embedF.set_footer(text="OT!zip")
-    await ctx.send(embed=embedF)
-    for root, dirs, files in os.walk("SQL/"+str(ctx.guild.id)):
+    ziph = zipfile.ZipFile("SQL/{0}/ZIP.zip".format(ctx.guild.id), 'w', zipfile.ZIP_DEFLATED)
+    embed=createEmbed("Le fichier .zip se génère.","Quand ce sera terminé, il vous sera envoyé par message privé.",0x220cc9,ctx.invoked_with.lower(),ctx.guild)
+    await ctx.send(embed=embed)
+    for root, dirs, files in os.walk("SQL/{0}".format(ctx.guild.id)):
         for file in files:
             ziph.write(os.path.join(root, file))
     ziph.close()
     embedF=discord.Embed(title="<:otVERT:718396570638483538> Voici les données de votre serveur.", description="Vous pouvez en refaire la demande quand vous le souhaitez.", color=0x339966)
     embedF.set_footer(text="OT!zip")
-    await ctx.author.send(embed=embedF,file=discord.File("_"+str(ctx.guild.id)+".zip"))
+    await ctx.author.send(embed=embedF,file=discord.File("SQL/{0}/ZIP.zip".format(ctx.guild.id)))
     return
