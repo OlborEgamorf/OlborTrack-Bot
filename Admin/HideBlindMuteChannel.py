645c328af2559863e0da740cd09c0c7639b45dbd
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-19 18:10:35 +0200

    Ajout possibilité de Hide/Blind/Mute des salons vocaux

--- Admin/HideBlindMuteChannel.py
+++ Admin/HideBlindMuteChannel.py
@@ -1,5 +1,6 @@
 from Stats.SQL.ConnectSQL import connectSQL
 from Core.Fonctions.Embeds import createEmbed, exeErrorExcept,embedAssert
+from Core.Fonctions.GetNom import getAuthor
 
 async def exeHBM(ctx,bot,option,guild):
     """Fonction qui permet de rendre masqué, bloqué ou muet des salons textuels pour un serveur.
@@ -16,17 +17,24 @@ async def exeHBM(ctx,bot,option,guild):
         dictEmbed={"Hide":"masqués","Blind":"bloqués","Mute":"muets"}
         dictBool={False:"",True:"dé"}
         descip=""
-        if ctx.message.channel_mentions==[]:
-            for i in curseur.execute("SELECT * FROM chans").fetchall():
-                if i[option]==True:
-                    descip+="<#"+str(i["ID"])+">, "
-        else:
+
+        if ctx.message.channel_mentions!=[]:
             for i in ctx.message.channel_mentions:
                 etat=curseur.execute("SELECT * FROM chans WHERE ID={0}".format(i.id)).fetchone()
                 curseur.execute("UPDATE chans SET {0}={1} WHERE ID={2}".format(option,bool(int(etat[option])-1),i.id))
-                descip+="<#"+str(i.id)+"> ({0}{1}), ".format(dictBool[etat[option]],dictEmbed[option][0:-1])
-            connexion.commit()
-            guild.getHBM()
+                descip+="<#{0}> ({1}{2}), ".format(i.id,dictBool[etat[option]],dictEmbed[option][0:-1])
+        elif len(ctx.args)>2:
+            chan=getAuthor("Voicechan",ctx,2)
+            etat=curseur.execute("SELECT * FROM chans WHERE ID={0}".format(chan)).fetchone()
+            curseur.execute("UPDATE chans SET {0}={1} WHERE ID={2}".format(option,bool(int(etat[option])-1),chan))
+            descip+="<#{0}> ({1}{2}), ".format(chan,dictBool[etat[option]],dictEmbed[option][0:-1])
+        else:
+            for i in curseur.execute("SELECT * FROM chans").fetchall():
+                if i[option]==True:
+                    descip+="<#"+str(i["ID"])+">, "
+
+        connexion.commit()
+        guild.getHBM()
         assert descip!="", "Aucun salon n'est {0}".format(dictEmbed[option][0:-1])
         embedTable=createEmbed("Salons "+dictEmbed[option],descip[0:-2],0x220cc9,ctx.invoked_with.lower(),ctx.guild)
     except AssertionError as er:
