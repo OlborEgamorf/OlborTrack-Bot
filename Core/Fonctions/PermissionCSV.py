f2e60fd1d4949d45a2d94196d16c65f267f70009
OlborEgamorf <lucas.triozon@orange.fr>
2021-07-19 18:45:37 +0200

    Suppression fichier obsolète

--- Core/Fonctions/PermissionCSV.py
+++
@@ -1,8 +0,0 @@
-### OBSOLETE, BIENTOT PLUS UTILISE MAIS QUAND MEME LA POUR LE MOMENT
-from Core.Fonctions.EcritureRecherche3 import rechercheCsv
-
-def checkperms(guild,numb):
-    tablePerms=rechercheCsv("perms",guild,0,0,0,0)[0]
-    assert tablePerms[numb]["Statut"]=="True", "Le module de stats lié à cette commande est désactivé sur ce serveur."
-    return
-#####
