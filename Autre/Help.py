661b36ae376532b1be995071d78c5b593ad5e957
OlborEgamorf <lucas.triozon@orange.fr>
2021-08-01 17:21:58 +0200

    Help compatible MP et raccourcis

--- Autre/Help.py
+++ Autre/Help.py
@@ -1,5 +1,3 @@
-### PAS ENCORE TERMINE, ASSIGNE A AUCUNE COMMANDE
-
 import discord
 from Core.Fonctions.Help3 import *
 from Core.Fonctions.AuteurIcon import auteur
@@ -7,9 +5,12 @@ from Stats.SQL.ConnectSQL import connectSQL
 from Core.Fonctions.setMaxPage import setPage
 from Core.Fonctions.Embeds import embedAssert, exeErrorExcept, sendEmbed
 
+dictDescip={"home":{},"stats":dictStats,"polls":dictPoll,"jeux":dictJeux,"utile":dictUtile,"autre":dictAutre,"sv":dictSV,"outils":dictOutils,"wiki":dictWiki,"mal":dictMAL,"admin":dictAdmin,"spotify":dictSpotify,"geo":dictGeo}
+
 async def commandeHelp(ctx,turn,react,ligne,bot,guildOT):
-    connexionCMD,curseurCMD=connectSQL(ctx.guild.id,"Commandes","Guild",None,None)
+    connexionCMD,curseurCMD=connectSQL(guildOT.id,"Commandes","Guild",None,None)
     if not react:
+        page=1
         if len(ctx.args)==2:
             option="home"
         else:
@@ -17,7 +18,18 @@ async def commandeHelp(ctx,turn,react,ligne,bot,guildOT):
                 option=ctx.args[2].lower()
             else:
                 option="home"
-        curseurCMD.execute("INSERT INTO commandes VALUES({0},{1},'help','{2}','None','None','None','None',1,1,'countDesc',False)".format(ctx.message.id,ctx.author.id,option))
+                if ctx.args[2].lower() in ("messganim","saloganim","emotganim","reacganim","motsganim","freqganim","voicganim","vchaganim"):
+                    option,page="stats",13
+                elif ctx.args[2].lower()=="getdata":
+                    option,page="admin",4
+                else:
+                    for i in dictDescip:
+                        for j in dictDescip[i]:
+                            for h in dictDescip[i][j]:
+                                if h==ctx.args[2].lower():
+                                    option,page=i,j
+                                    break
+        curseurCMD.execute("INSERT INTO commandes VALUES({0},{1},'help','{2}','None','None','None','None',{3},99,'countDesc',False)".format(ctx.message.id,ctx.author.id,option,page))
         ligne=curseurCMD.execute("SELECT * FROM commandes WHERE MessageID={0}".format(ctx.message.id)).fetchone()
     else:
         option=ligne["Option"]
@@ -34,7 +46,6 @@ def embedHelp30(option,guildOT,page,bot):
     dictAuthor={"home":"Bienvenue sur la page d’aide de Olbor Track !","stats":"Aide - Statistiques","polls":"Aide - Sondages, giweaway et rappels","jeux":"Aide - Jeux","utile":"Aide - Commandes utiles","autre":"Aide - Autres commandes","sv":"Aide - Boite de connaissances","outils":"Aide - Outils","wiki":"Aide - Wikipedia","mal":"Aide - MyAnimeList","admin":"Aide - Commandes admin","spotify":"Aide - Spotify","geo":"Aide - Géographie et Espace"}
     dictLen={"home":3,"stats":len(dictStats),"polls":len(dictPoll),"jeux":len(dictJeux),"utile":len(dictUtile),"autre":len(dictAutre),"sv":len(dictSV),"outils":len(dictOutils),"wiki":len(dictWiki),"mal":len(dictMAL),"admin":len(dictAdmin),"spotify":len(dictSpotify),"geo":len(dictGeo)}
 
-    dictDescip={"home":{},"stats":dictStats,"polls":dictPoll,"jeux":dictJeux,"utile":dictUtile,"autre":dictAutre,"sv":dictSV,"outils":dictOutils,"wiki":dictWiki,"mal":dictMAL,"admin":dictAdmin,"spotify":dictSpotify,"geo":dictGeo}
     dictDescipPlus={"home":{},"stats":dictPStats,"polls":dictPPoll,"jeux":dictPJeux,"utile":dictPUtile,"autre":dictPAutre,"sv":dictPSV,"outils":dictPOutils,"wiki":dictPWiki,"mal":dictPMAL,"admin":dictPAdmin,"spotify":dictPSpotify,"geo":dictPGeo}
     dictDescipFields={"home":{},"stats":dictFStats,"polls":dictFPoll,"jeux":dictFJeux,"utile":dictFUtile,"autre":dictFAutre,"sv":dictFSV,"outils":dictFOutils,"wiki":dictFWiki,"mal":dictFMAL,"admin":dictFAdmin,"spotify":dictFSpotify,"geo":dictFGeo}
     dictDescipTitres={"home":{},"stats":dictTStats,"polls":dictTPoll,"jeux":dictTJeux,"utile":dictTUtile,"autre":dictTAutre,"sv":dictTSV,"outils":dictTOutils,"wiki":dictTWiki,"mal":dictTMAL,"admin":dictTAdmin,"spotify":dictTSpotify,"geo":dictTGeo}
@@ -46,9 +57,9 @@ def embedHelp30(option,guildOT,page,bot):
     embedHelp=discord.Embed(color=dictColor[option])
     embedHelp.set_author(icon_url=dictLinks[option],name=dictAuthor[option])
     if option=="home":
-        embedHelp.description="Effectuez une de ces commandes pour avoir plus d'infos sur mes commandes !\n**Mon préfixe est OT!**\n[Invitez moi !](https://bit.ly/OlborTrack) - [Faites une donation !](https://paypal.me/OlborTrack) - [Serveur de test](https://discord.com/invite/kMQz7nF) - [Twitter](https://twitter.com/olbortrack) - [Instagram](https://www.instagram.com/OlborTrack/)"
+        embedHelp.description="Effectuez une de ces commandes pour avoir plus d'infos sur mes commandes !\n**Mon préfixe est OT!**\nVous pouvez aussi faire **OT!help [nom d'une commande]** pour avoir directement les infos d'une commande.\n[Invitez moi !](https://discord.com/oauth2/authorize?client_id=699728606493933650&permissions=120259472576&scope=bot) - [Faites une donation !](https://paypal.me/OlborTrack) - [Serveur de test](https://discord.com/invite/kMQz7nF) - [Twitter](https://twitter.com/olbortrack) - [Instagram](https://www.instagram.com/OlborTrack/)"
         if page==1 or page==2:
-            if guildOT!=None:
+            if guildOT.mcmd!=None:
                 dictPerms={"Stats":0,"Sondages":1,"Outils":2,"Savezvous":3,"Jeux":4,"MAL":7,"Wiki":8,"Spotify":9,"Geo":10}
                 nb=0
                 for i in guildOT.mcmd:
