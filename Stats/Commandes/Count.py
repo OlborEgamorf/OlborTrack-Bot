5d0a42aa9e4ec1a88ddea2e17e32d5f11e1db68f
OlborEgamorf <lucas.triozon@orange.fr>
2021-09-13 21:44:32 +0200

    Ajout commandes résumé pour les jeux

--- Stats/Commandes/Count.py
+++ Stats/Commandes/Count.py
@@ -2,6 +2,8 @@ from time import strftime
 from Stats.SQL.ConnectSQL import connectSQL
 from Core.Fonctions.Embeds import createEmbed, defEvol
 from Core.Fonctions.TempsVoice import tempsVoice
+from Titres.Badges import getBadges
+from Core.Fonctions.GetNom import getTitre
 
 tableauMois={"01":"Janvier","02":"Février","03":"Mars","04":"Avril","05":"Mai","06":"Juin","07":"Juillet","08":"Aout","09":"Septembre","10":"Octobre","11":"Novembre","12":"Décembre","TO":"Année","janvier":"01","février":"02","mars":"03","avril":"04","mai":"05","juin":"06","juillet":"07","aout":"08","septembre":"09","octobre":"10","novembre":"11","décembre":"12","glob":"GL","to":"TO"}
 
@@ -33,6 +35,73 @@ async def commandeCount(ctx):
             if mois!=None:
                 count=tempsVoice(mois["Count"]) if i=="Voice" else mois["Count"]
                 descip+="**{0} 20{1}** : {2} - {3}e {4}\n".format(tableauMois[strftime("%m")],strftime("%y"),count,mois["Rank"],defEvol(mois,True))
+        except:
+            pass
+        
+        if descip!="":
+            embed.add_field(name=i,value=descip,inline=True)
+    await ctx.reply(embed=embed)
+
+
+async def jeuxPerso(ctx):
+    embed=createEmbed("Tableau jeux - perso","",ctx.author.color.value,ctx.invoked_with.lower(),ctx.author)
+    for i in ["P4","BatailleNavale","Tortues","TortuesDuo","TrivialVersus","TrivialParty","TrivialBR","Matrice"]:
+        descip=""
+        try:
+            connexion,curseur=connectSQL(ctx.guild.id,i,"Jeux","GL","")
+            glob=curseur.execute("SELECT * FROM glob WHERE ID={0}".format(ctx.author.id)).fetchone()
+            if glob!=None:
+                descip+="**Global** : {0} - {1}e {2}\n".format(glob["Count"],glob["Rank"],defEvol(glob,True))
+        except:
+            pass
+
+        try:
+            connexion,curseur=connectSQL(ctx.guild.id,i,"Jeux","TO",strftime("%y"))
+            annee=curseur.execute("SELECT * FROM to{0} WHERE ID={1}".format(strftime("%y"),ctx.author.id)).fetchone()
+            if annee!=None:
+                descip+="**20{0}** : {1} - {2}e {3}\n".format(strftime("%y"),annee["Count"],annee["Rank"],defEvol(annee,True))
+        except:
+            pass
+
+        try:
+            connexion,curseur=connectSQL(ctx.guild.id,i,"Jeux",strftime("%m"),strftime("%y"))
+            mois=curseur.execute("SELECT * FROM {0}{1} WHERE ID={2}".format(tableauMois[strftime("%m")].lower(),strftime("%y"),ctx.author.id)).fetchone()
+            if mois!=None:
+                descip+="**{0} 20{1}** : {2} - {3}e {4}\n".format(tableauMois[strftime("%m")],strftime("%y"),mois["Count"],mois["Rank"],defEvol(mois,True))
+        except:
+            pass
+        
+        if descip!="":
+            embed.add_field(name=i,value=descip,inline=True)
+    await ctx.reply(embed=embed)
+
+
+async def jeuxBoard(ctx,bot):
+    embed=createEmbed("Tableau jeux - premiers","",0x3498db,ctx.invoked_with.lower(),bot.user)
+    connexionTitres,curseurTitres=connectSQL("OT","Titres","Titres",None,None)
+    for i in ["P4","Tortues","TortuesDuo","TrivialVersus","TrivialParty","TrivialBR","Matrice","BatailleNavale"]:
+        descip=""
+        try:
+            connexion,curseur=connectSQL("OT",i,"Jeux","GL","")
+            glob=curseur.execute("SELECT * FROM glob WHERE Rank=1").fetchone()
+            if glob!=None:
+                descip+="**Global** : {0} {1} - {2} points\n".format(getBadges(glob["ID"],i),getTitre(curseurTitres,glob["ID"]),glob["Count"])
+        except:
+            pass
+
+        try:
+            connexion,curseur=connectSQL("OT",i,"Jeux","TO",strftime("%y"))
+            annee=curseur.execute("SELECT * FROM to{0} WHERE Rank=1".format(strftime("%y"))).fetchone()
+            if annee!=None:
+                descip+="**20{0}** : {1} {2} - {3} points\n".format(strftime("%y"),getBadges(annee["ID"],i),getTitre(curseurTitres,annee["ID"]),annee["Count"])
+        except:
+            pass
+
+        try:
+            connexion,curseur=connectSQL("OT",i,"Jeux",strftime("%m"),strftime("%y"))
+            mois=curseur.execute("SELECT * FROM {0}{1} WHERE Rank=1".format(tableauMois[strftime("%m")].lower(),strftime("%y"))).fetchone()
+            if mois!=None:
+                descip+="**{0} 20{1}** : {2} {3} - {4} points\n".format(tableauMois[strftime("%m")],strftime("%y"),getBadges(mois["ID"],i),getTitre(curseurTitres,mois["ID"]),mois["Count"])
         except:
             pass
         
--- Titres/Badges.py
+++ Titres/Badges.py
@@ -1,35 +1,39 @@
+from Core.Fonctions.Embeds import createEmbed, embedAssert
 from Stats.SQL.ConnectSQL import connectSQL
-from Core.Fonctions.Embeds import createEmbed
+
 dictValues={1:"<:BronzeMois:886707210749632542>",2:"<:ArgentMois:886707209797521448>",3:"<:OrMois:886707210472800276>",11:"<:BronzeAnnee:886707210560884776>",12:"<:ArgentAnnee:886707209965277225>",13:"<:OrAnnee:886707210430840862>",101:"<:SaphirGlobal:886707210409893958>",102:"<:RubisGlobal:886707210225332255>",103:"<:DiamantGlobal:886707210560884778>"}
 
 async def showBadges(ctx,bot):
-    connexionUser,curseurUser=connectSQL("OT",ctx.author.id,"Titres",None,None)
-    liste=["P4","BatailleNavale","Tortues","TortuesDuo","TrivialVersus","TrivialParty","TrivialBR","Matrice"]
-    liste.sort()
-    listeValues=[1,2,3,11,12,13]
-    descip=""
-    for i in liste:
-        descip+="**{0}** : ".format(i)
-        for j in listeValues:
-            if curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND Valeur={1}".format(i,j)).fetchone()==None:
-                descip+="<:BadgeVide:886707210003025982> "
-            else:
-                descip+="{0} ".format(dictValues[j])
-        if curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND (Valeur=101 OR Valeur=102 OR Valeur=103)".format(i,j)).fetchone()!=None:
-            descip+="{0} ".format(dictValues[curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND (Valeur=101 OR Valeur=102 OR Valeur=103)".format(i,j)).fetchone()["Valeur"]])
-        descip+="\n"
+    try:
+        connexionUser,curseurUser=connectSQL("OT",ctx.author.id,"Titres",None,None)
+        liste=["P4","BatailleNavale","Tortues","TortuesDuo","TrivialVersus","TrivialParty","TrivialBR","Matrice"]
+        liste.sort()
+        listeValues=[1,2,3,11,12,13]
+        descip=""
+        for i in liste:
+            descip+="**{0}** : ".format(i)
+            for j in listeValues:
+                if curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND Valeur={1}".format(i,j)).fetchone()==None:
+                    descip+="<:BadgeVide:886707210003025982> "
+                else:
+                    descip+="{0} ".format(dictValues[j])
+            if curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND (Valeur=101 OR Valeur=102 OR Valeur=103)".format(i,j)).fetchone()!=None:
+                descip+="{0} ".format(dictValues[curseurUser.execute("SELECT * FROM badges WHERE Type='{0}' AND (Valeur=101 OR Valeur=102 OR Valeur=103)".format(i,j)).fetchone()["Valeur"]])
+            descip+="\n"
+            
+        if curseurUser.execute("SELECT * FROM badges WHERE Période='VIP'").fetchone()==None:
+            descip+="\n***VIP*** : <:VIPVide:886707613260193853>\n"
+        else:
+            descip+="\n***VIP*** : <:VIP:886707613667049533>\n"
         
-    if curseurUser.execute("SELECT * FROM badges WHERE Période='VIP'").fetchone()==None:
-        descip+="\n***VIP*** : <:VIPVide:886707613260193853>\n"
-    else:
-        descip+="\n***VIP*** : <:VIP:886707613667049533>\n"
-    
-    if curseurUser.execute("SELECT * FROM badges WHERE Période='Testeur'").fetchone()==None:
-        descip+="***Testeur*** : <:TesteurVide:886707613260218398>\n"
-    else:
-        descip+="***Testeur*** : <:Testeur:886707613792862250>\n"
+        if curseurUser.execute("SELECT * FROM badges WHERE Période='Testeur'").fetchone()==None:
+            descip+="***Testeur*** : <:TesteurVide:886707613260218398>\n"
+        else:
+            descip+="***Testeur*** : <:Testeur:886707613792862250>\n"
 
-    await ctx.reply(embed=createEmbed("Vos badges",descip,ctx.author.color.value,"{0} {1}".format(ctx.invoked_parents[0],ctx.invoked_with.lower()),ctx.author))
+        await ctx.reply(embed=createEmbed("Vos badges",descip,ctx.author.color.value,"{0} {1}".format(ctx.invoked_parents[0],ctx.invoked_with.lower()),ctx.author))
+    except:
+        await ctx.reply(embed=embedAssert("Vous n'avez aucun badge visiblement..."))
 
 
 def getBadges(user,jeu):
